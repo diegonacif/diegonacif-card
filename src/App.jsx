eeca36ba68d6deb86b74b21c2ba6250c2306b0f4
diegonacif <diegoreisnacif@gmail.com>
2022-05-29 18:04:40 -0300

    created folder structure, add globalStyles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Dashboard } from './pages/Dashboard/Dashboard';
+import GlobalStyle from './styles/globalStyles'
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Dashboard />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import GlobalStyle from "../../styles/globalStyles";
+
+export function Header() {
+  return (
+    <>
+      <h1>Diego Nacif</h1>
+      <div>
+        <span>Sobre</span>
+        <span>Projetos</span>
+        <span>Tecnologias</span>
+        <button>Contato</button>
+      </div>  
+    </>  
+  );
+}
